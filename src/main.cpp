d09812ca6fd7896b82634cdcda4dafcc88fd9dc9
Peter Wells <petethebeatwells@gmail.com>
2020-08-15 14:06:50 -0700

    put in the code to actually render a window!

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,77 @@
+#include <iostream>
+
 // external libs
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
+// declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 
-int main() {
+int main() 
+{
 	// create a GLFW window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // <== this is for MAC OS. We are Windows only for now
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Pete Learns OpenGL", NULL, NULL);
+	if (window == NULL) 
+	{
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// init GLAD (glad manages function pointers for OpenGL)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;	
+	}
+
+	// match OpenGL's window to the GLFW one. Could be smaller if we wanted to make superman 64 :P
+	glViewport(0, 0, 800, 600);
+	// tell GLFW to call the resize-callback when changing the window size so that OpenGL's viewport matches GLFW
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//  THE RENDER LOOP
+	// |---------------|
+	while (!glfwWindowShouldClose(window))
+	{
+		// check input
+		processInput(window);
+
+		// rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// clean up
+	glfwTerminate();
 
 	return 0;
+}
+
+
+// resizing window callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// handle input
+void processInput(GLFWwindow *window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
